e703b64570216e9b9bccf43e925c90caddbcfc55
RishaubKumar <146576589+RishaubKumar@users.noreply.github.com>
2026-08-08 02:13:16 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+        for(int i =0;i<n-1;i++){
+            Arrays.sort(stones);
+            stones[n-1] = stones[n-1] - stones[n-2];
+            stones[n-2] = 0;
+        }
+        Arrays.sort(stones);
+        return stones[n-1];
+    }
+}
